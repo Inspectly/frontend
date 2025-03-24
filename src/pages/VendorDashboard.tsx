3a31c2d28eb60f9f94c90550df246f2085cd3e2c
Matt Kikuchi <mattkikuchi@hotmail.com>
2025-03-24 03:08:10 -0400

    update layout

--- src/pages/VendorDashboard.tsx
+++ src/pages/VendorDashboard.tsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { CalendarEvent, User } from "../types";
+import { CalendarEvent, IssueType, User } from "../types";
 import UserCalendar from "../components/UserCalendar";
 import { useGetListingsQuery } from "../features/api/listingsApi";
 import { useGetReportsQuery } from "../features/api/reportsApi";
 import { useGetIssuesQuery } from "../features/api/issuesApi";
+import { Link } from "react-router-dom";
+import DashboardCharts from "../components/DashboardCharts";
 
 interface DashboardProps {
   user: User;
@@ -42,6 +44,10 @@ const Dashboard: React.FC<DashboardProps> = ({ user }) => {
     }
   }, [selectedReport, refetchIssues]);
 
+  const filteredIssues = selectedReport === "all"
+        ? issues || []
+        : issues?.filter((issue: IssueType) => issue.report_id.toString() === selectedReport) || [];
+
   useEffect(() => {
     const dummyBookings: CalendarEvent[] = [
       {
@@ -75,12 +81,12 @@ const Dashboard: React.FC<DashboardProps> = ({ user }) => {
   return (
     <div className="p-6">
       <div className="flex flex-wrap items-center gap-2 mb-6">
-        <h1 className="text-2xl font-semibold mb-0">Vendor Dashboard</h1>
+        <h1 className="text-2xl font-semibold mb-0">Hello username!</h1>
       </div>
       <div className="gap-6 grid grid-cols-1 2xl:grid-cols-12">
         <div className="col-span-12 2xl:col-span-8">
           <div className="gap-6 grid grid-cols-1 sm:grid-cols-12">
-          <div className="col-span-12">
+            <div className="col-span-12">
               <div className="nft-promo-card card border-0 rounded-xl overflow-hidden relative z-1 py-6 3xl:px-[76px] 2xl:px-[56px] xl:px-[40px] lg:px-[28px] px-4">
                 <img
                   src="/images/gradient-bg.png"
@@ -106,16 +112,92 @@ const Dashboard: React.FC<DashboardProps> = ({ user }) => {
                 </div>
               </div>
             </div>
-            <div className="flex-grow-2 col-span-12">
-              <span>Terms & Conditions</span>
-              <span>Hello, (Contractor Name)!</span>
-            </div>
-            <div className="col-span-12">
-              <div className="rounded-md bg-white h-full">
-                <UserCalendar events={events} />
+          </div>
+        </div>
+      </div>
+      <h6 className="mb-4 font-semibold">
+        Current Bids & Active Jobs
+      </h6>
+      <div className="col-span-12 2xl:col-span-4">
+        <div className="gap-6 grid grid-cols-1 sm:grid-cols-12">
+          <div className="col-span-12 md:col-span-6 2xl:col-span-12">
+            <div className="rounded-lg border-gray-600 bg-white h-full">
+              <div className="border-b border-gray-200 px-4 py-3 border-bottom flex items-center flex-wrap gap-2 justify-between">
+                <h6 className="font-bold text-lg mb-0">Recent Bids</h6>
+                {/* <a
+                  href={`/dashboard/${selectedListing}`}
+                  className="text-blue-400 hover:text-blue-500 flex items-center gap-1"
+                >
+                  View All
+                </a> */}
+              </div>
+              <div className="card-body px-6 pb-6">
+                <div>
+                  {Object.entries(
+                    filteredIssues.reduce((acc, issue) => {
+                      if (!acc[issue.report_id]) acc[issue.report_id] = [];
+                      acc[issue.report_id].push(issue);
+                      return acc;
+                    }, {} as Record<string, IssueType[]>)
+                  ).map(([reportId, issueArray]) =>
+                    issueArray.map(
+                      (issue) =>
+                        issue.cost && (
+                          <div
+                            key={issue.id}
+                            className="flex items-center justify-between gap-2 mt-6"
+                          >
+                            <div className="flex items-center gap-3">
+                              <img
+                                src={`/images/${issue.type.toLowerCase()}.png`}
+                                alt=""
+                                className="w-10 h-10 shrink-0 overflow-hidden"
+                              />
+                              <div className="grow">
+                                <h6 className="text-base mb-0 font-medium">
+                                  <Link
+                                    to={`/dashboard/${issue.report_id}/issue/${issue.id}?tab=bids`}
+                                    className="text-blue-400 hover:underline"
+                                  >
+                                    {issue.id} {issue.type}
+                                  </Link>
+                                </h6>
+                                <span className="text-sm font-medium">
+                                  {issue.summary}
+                                </span>
+                              </div>
+                            </div>
+                            <span className="text-neutral-600 text-base font-medium">
+                              {issue.cost}
+                            </span>
+                          </div>
+                        )
+                    )
+                  )}
+                </div>
               </div>
             </div>
           </div>
+          {/* <div className="col-span-12 md:col-span-6 2xl:col-span-12">
+            <DashboardCharts />
+          </div> */}
+        </div>
+      </div>
+      <h6 className="mb-4 font-semibold">
+        Active Jobs
+      </h6>
+      {/* todo: find active jobs component... */}
+      {/* <div className="col-span-12">
+        <div className="rounded-md bg-white h-full">
+          <UserCalendar events={events} />
+        </div>
+      </div> */}
+      <h6 className="mb-4 font-semibold">
+        Upcoming Assessments/Repairs
+      </h6>
+      <div className="col-span-12">
+        <div className="rounded-md bg-white h-full">
+          <UserCalendar events={events} />
         </div>
       </div>
     </div>
