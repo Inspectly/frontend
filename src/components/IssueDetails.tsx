7f709a2c6169055c597826e827d5b50ce3aee831
Abid Al Labib <getabidallabib@gmail.com>
2025-12-07 21:45:11 -0500

    review feature implemented

--- src/components/IssueDetails.tsx
+++ src/components/IssueDetails.tsx
@@ -383,9 +383,8 @@ const IssueDetails: React.FC<IssueDetailsProps> = ({ issue, listing }) => {
       className="relative rounded-lg bg-white border-0 overflow-hidden flex flex-col"
     >
       <div
-        className={`items-center px-6 py-4 active border-b border-neutral-200 ${
-          issue.active ? "" : "bg-red-100"
-        }`}
+        className={`items-center px-6 py-4 active border-b border-neutral-200 ${issue.active ? "" : "bg-red-100"
+          }`}
       >
         <div className="flex flex-row items-center justify-between">
           <h2 className="text-2xl font-medium mb-0">
@@ -418,11 +417,10 @@ const IssueDetails: React.FC<IssueDetailsProps> = ({ issue, listing }) => {
         >
           <li role="presentation">
             <button
-              className={`inline-block px-4 py-2.5 font-semibold border-b-2 rounded-t-lg ${
-                activeTab === "details"
+              className={`inline-block px-4 py-2.5 font-semibold border-b-2 rounded-t-lg ${activeTab === "details"
                   ? "text-blue-600 border-blue-600"
                   : "text-gray-500 hover:text-gray-600 border-gray-100 hover:border-gray-300"
-              }`}
+                }`}
               type="button"
               role="tab"
               aria-controls="default-details"
@@ -434,11 +432,10 @@ const IssueDetails: React.FC<IssueDetailsProps> = ({ issue, listing }) => {
           </li>
           <li role="presentation">
             <button
-              className={`inline-block px-4 py-2.5 relative font-semibold border-b-2 rounded-t-lg ${
-                activeTab === "offers"
+              className={`inline-block px-4 py-2.5 relative font-semibold border-b-2 rounded-t-lg ${activeTab === "offers"
                   ? "text-blue-600 border-blue-600"
                   : "text-gray-500 hover:text-gray-600 border-gray-100 hover:border-gray-300"
-              }`}
+                }`}
               type="button"
               role="tab"
               aria-controls="default-offers"
@@ -453,11 +450,10 @@ const IssueDetails: React.FC<IssueDetailsProps> = ({ issue, listing }) => {
           </li>
           <li role="presentation">
             <button
-              className={`inline-block px-4 py-2.5 font-semibold border-b-2 rounded-t-lg ${
-                activeTab === "assessments"
+              className={`inline-block px-4 py-2.5 font-semibold border-b-2 rounded-t-lg ${activeTab === "assessments"
                   ? "text-blue-600 border-blue-600"
                   : "text-gray-500 hover:text-gray-600 border-gray-100 hover:border-gray-300"
-              }`}
+                }`}
               type="button"
               role="tab"
               aria-controls="default-assessments"
@@ -567,18 +563,17 @@ const IssueDetails: React.FC<IssueDetailsProps> = ({ issue, listing }) => {
                           Progress
                         </h4>
                         <button
-                          className={`px-2.5 py-1.5 rounded font-medium text-sm ${
-                            statusMapping[issue.status as IssueStatus] ===
-                            "open"
+                          className={`px-2.5 py-1.5 rounded font-medium text-sm ${statusMapping[issue.status as IssueStatus] ===
+                              "open"
                               ? "bg-neutral-100 text-neutral-600 border border-neutral-600"
                               : statusMapping[issue.status as IssueStatus] ===
                                 "in_progress"
-                              ? "bg-blue-100 text-blue-600 border border-blue-600"
-                              : statusMapping[issue.status as IssueStatus] ===
-                                "review"
-                              ? "bg-yellow-100 text-yellow-600 border border-yellow-600"
-                              : "bg-green-100 text-green-600 border border-green-600"
-                          }`}
+                                ? "bg-blue-100 text-blue-600 border border-blue-600"
+                                : statusMapping[issue.status as IssueStatus] ===
+                                  "review"
+                                  ? "bg-yellow-100 text-yellow-600 border border-yellow-600"
+                                  : "bg-green-100 text-green-600 border border-green-600"
+                            }`}
                           ref={progressDropdownButtonRef}
                           onClick={() =>
                             setProgressDropdownOpen((prev) =>
@@ -606,12 +601,11 @@ const IssueDetails: React.FC<IssueDetailsProps> = ({ issue, listing }) => {
                               {statusOptions.map(({ value, label }) => (
                                 <button
                                   key={value}
-                                  className={`block px-4 py-2 text-sm hover:bg-gray-100 w-full text-left ${
-                                    `Status.${value.toUpperCase()}` ===
-                                    issue.status
+                                  className={`block px-4 py-2 text-sm hover:bg-gray-100 w-full text-left ${`Status.${value.toUpperCase()}` ===
+                                      issue.status
                                       ? "font-bold"
                                       : ""
-                                  }`}
+                                    }`}
                                   onClick={(e) => {
                                     e.stopPropagation();
                                     handleStatusChange(issue.id, value);
@@ -629,13 +623,12 @@ const IssueDetails: React.FC<IssueDetailsProps> = ({ issue, listing }) => {
                           Severity
                         </h4>
                         <p
-                          className={`text-base font-semibold ${
-                            issue.severity === "High"
+                          className={`text-base font-semibold ${issue.severity === "High"
                               ? "text-red-600"
                               : issue.severity === "Medium"
-                              ? "text-yellow-600"
-                              : "text-green-600"
-                          }`}
+                                ? "text-yellow-600"
+                                : "text-green-600"
+                            }`}
                         >
                           {issue.severity}
                         </p>
@@ -868,27 +861,27 @@ const IssueDetails: React.FC<IssueDetailsProps> = ({ issue, listing }) => {
               <p>Error loading offers</p>
             ) : (
               <>
-                  {userType === "client" && (
-                    <OffersTabClient
-                      offers={offers}
-                      uniqueVendors={uniqueVendors}
-                      handleOpenOfferModal={handleOpenOfferModal}
-                      onOpenOfferModal={() => setIsOfferModalOpen(true)}
-                      onOfferAccepted={async (acceptedOffer) => {
-                        try {
-                          const response = await createCheckoutSession({
-                            client_id: userId,
-                            vendor_id: acceptedOffer.vendor_id,
-                            offer_id: acceptedOffer.id,
-                          }).unwrap();
-                          window.location.href = response.session_url;
-                        } catch (err) {
-                          console.error("Stripe error", err);
-                          alert("Could not start payment, please try again.");
-                        }
-                      }}
-                    />
-                  )}
+                {userType === "client" && (
+                  <OffersTabClient
+                    offers={offers}
+                    uniqueVendors={uniqueVendors}
+                    handleOpenOfferModal={handleOpenOfferModal}
+                    onOpenOfferModal={() => setIsOfferModalOpen(true)}
+                    onOfferAccepted={async (acceptedOffer) => {
+                      try {
+                        const response = await createCheckoutSession({
+                          client_id: userId,
+                          vendor_id: acceptedOffer.vendor_id,
+                          offer_id: acceptedOffer.id,
+                        }).unwrap();
+                        window.location.href = response.session_url;
+                      } catch (err) {
+                        console.error("Stripe error", err);
+                        alert("Could not start payment, please try again.");
+                      }
+                    }}
+                  />
+                )}
 
                 {userType === "vendor" && (
                   <OffersTabVendor
